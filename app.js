fcd1eea27067bc9faf128a5143c2109e9721f478
Janith Olagama <janitholagama.info@gmail.com>
2018-11-17 23:06:43 +0400

    Add init function
    Add init function to invoke all the event listeners on the page load.

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ var budgetController = (function () {
 })();
 
 //UI CONTROLLER IIFE
-var uiController =  (function () {
+var uiController = (function () {
 
     //Declaring an object for all the class names and IDs
     var names = {
         inputType: ".add__type",
         description: ".add__description",
         value: ".add__value",
-        addButton : ".add__btn"
+        addButton: ".add__btn"
     };
 
     //This function will return an object and will assign that object to the uiController variable.
     return {
-        getInput : function () {
+        getInput: function () {
             //We return an object here as well, As it will be easy to access
             return {
-                type : document.querySelector (names.inputType).value, // Return will be either inc (for Income) or exp (For expense)
-                description : document.querySelector (names.description).value,
-                value : document.querySelector (names.value).value
-            } 
+                type: document.querySelector(names.inputType).value, // Return will be either inc (for Income) or exp (For expense)
+                description: document.querySelector(names.description).value,
+                value: document.querySelector(names.value).value
+            }
         },
 
-        getNames : function () {
+        getNames: function () {
             return names;
         }
     };
@@ -34,12 +34,28 @@ var uiController =  (function () {
 //GLOBAL APP Controller
 var globalController = (function (bdgCtrl, uiCtrl) {
 
-    var names = uiCtrl.getNames ();
+    //Add event listner function, All the event listners goes here and this function is invoked in the init function, will setup all event listners
+    var setupEventListners = function () {
+        //Access DOM Class and ID Names
+        var names = uiCtrl.getNames();
+
+        //Adding the event listner for the button
+        document.querySelector(names.addButton).addEventListener("click", ctrlAddItems);
+
+        //Add the enter key press event
+        document.addEventListener("keypress", function (event) {
+            //Event listner was added to the entire document not to specific html controller
+            //As this key press event can happen any where in the document
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItems();
+            };
+        });
+    };
 
     var ctrlAddItems = function () {
         //Task one - Get the input data
-        var input = uiCtrl.getInput ();
-        console.log (input);
+        var input = uiCtrl.getInput();
+        console.log(input);
 
         //Task two -  Add items to the budgte data
 
@@ -48,16 +64,14 @@ var globalController = (function (bdgCtrl, uiCtrl) {
         //Display the budget
     };
 
-    //Addind the event listner for the button
-    document.querySelector (names.addButton).addEventListener ("click", ctrlAddItems);
-    
-    //Add the enter key press event
-    document.addEventListener ("keypress", function (event) {
-        //Event listner was added to the entire document not to specific html controller
-        //As this key press event can happen any where in the document
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItems ();
+    return {
+        init : function () {
+            console.log ("Application as started")
+            setupEventListners ();
         }
-    });
+    };
 
 })(budgetController, uiController);
+
+//Calling the init function
+globalController.init ();
