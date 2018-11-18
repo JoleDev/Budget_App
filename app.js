5b72435eb28230d946d7433d616008a35b6ebe57
Janith Olagama <janitholagama.info@gmail.com>
2018-11-18 10:46:23 +0400

    Add the functionality to budget controller
    Add the functionality to budget controller to handle return from input function and store the data in the data object in budget controller

--- app.js
+++ app.js
@@ -1,6 +1,59 @@
 //BUDGET CONTROLLER IIFE
 var budgetController = (function () {
 
+    //Function constructors was created, to update Income and Expense items
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    //Object based data structutre used to store the data
+    var data = {
+        allItems : {
+            expense : [],
+            income : []
+        },
+        totals : {
+            expense : 0,
+            income : 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, id;
+
+            //Create new ID
+            if (data.allItems[type].length > 0) {
+                id = data.allItems[type][data.allItems[type].length-1].id + 1;
+            } else {
+                id = 0;
+            }
+            
+            //Create new item based on the type, Income or Expense
+            if (type === "income") {
+                newItem = new Income (id, des, val);
+            } else if (type === "expense") {
+                newItem = new Expense (id, des, val);
+            }
+
+            //Puch the new data to the data array defined above
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+
+        testing: function () {
+            console.log (data);
+        }
+    }
+
 })();
 
 //UI CONTROLLER IIFE
@@ -19,7 +72,7 @@ var uiController = (function () {
         getInput: function () {
             //We return an object here as well, As it will be easy to access
             return {
-                type: document.querySelector(names.inputType).value, // Return will be either inc (for Income) or exp (For expense)
+                type: document.querySelector(names.inputType).value, // Return will be either income (for Income) or expense (For expense)
                 description: document.querySelector(names.description).value,
                 value: document.querySelector(names.value).value
             }
@@ -53,11 +106,13 @@ var globalController = (function (bdgCtrl, uiCtrl) {
     };
 
     var ctrlAddItems = function () {
+        var input, newItem;
+
         //Task one - Get the input data
-        var input = uiCtrl.getInput();
-        console.log(input);
+        input = uiCtrl.getInput();
 
         //Task two -  Add items to the budgte data
+        newItem = bdgCtrl.addItem (input.type, input.description, input.value);
 
         //Task three - Calculate the budget value
 
